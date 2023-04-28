4a5ab2901fbaf516b3f05791496c21a45de64399
canitzp <canitzp@gmail.com>
2023-04-28 14:43:35 +0200

    Fixed removal recipe. Any helmet with any nbt tag, could produce infinite modules

--- src/main/java/de/canitzp/solarhelmet/SolarRecipeManager.java
+++ src/main/java/de/canitzp/solarhelmet/SolarRecipeManager.java
@@ -83,7 +83,7 @@ public class SolarRecipeManager {
                     return false; // this "should" never happen
                 }
                 if (!inputStack.hasTag()) {
-                    return true;
+                    return false;
                 }
                 return inputStack.getTag().getBoolean("SolarHelmet");
             }
